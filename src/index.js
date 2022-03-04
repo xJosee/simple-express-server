f6d7cf210ff5d85f5dd2626cae285f4fe7a2e7de
Jose Luis <54871810+xJosee@users.noreply.github.com>
2022-03-04 15:56:21 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Software Avanzado - Tarea Práctica 5 - 201807431 - José Luis Herrera</h1>');
+    res.json({ message: "Software Avanzado - Tarea Práctica 5 - 201807431 - José Luis Herrera" });
 });
 
 app.listen(PORT, () => {
